c61922012ea5cb6ebf361bea6f913d861a0de9a0
Peter Entwistle <petertheanimator@gmail.com>
2017-03-03 15:38:34 +0000

    Added source files to Sources directory for package manager.

---
+++ Sources/MBR.cpp
@@ -0,0 +1,36 @@
+//
+//  MBR.cpp
+//  OpenCVTest
+//
+//  Created by Peter Entwistle on 31/10/2016.
+//  Copyright © 2016 Peter Entwistle. All rights reserved.
+//
+/*
+#include "MBR.hpp"
+
+using namespace std;
+
+const void * initializeTest(char *filename) {
+	MBR *mbr = new MBR(filename);
+	
+	return (void *)mbr;
+}
+
+const char *hexdump(const void *object) {
+	MBR *mbr;
+	static char retval[2048];
+	
+	mbr = (MBR *)object;
+	strcpy(retval, mbr -> hexdump());
+	return retval;
+}
+
+const char *imageType(const void *object) {
+	MBR *mbr;
+	static char retval[256];
+	
+	mbr = (MBR *)object;
+	strcpy(retval, mbr -> imageType());
+	return retval;
+}
+*/
---
+++ Sources/MBR.hpp
@@ -0,0 +1,29 @@
+//
+//  MBR.hpp
+//  OpenCVTest
+//
+//  Created by Peter Entwistle on 31/10/2016.
+//  Copyright © 2016 Peter Entwistle. All rights reserved.
+//
+/*
+#ifndef MBR_hpp
+#define MBR_hpp
+
+#include <stdio.h>
+#include <string>
+
+class MBR {
+	std::string filename;
+	
+public:
+	MBR (std::string filename);
+	const char *hexdump();
+	const char *imageType();
+	const char *bootCode();
+	const char *partitions();
+private:
+	bool readFile(unsigned char *buffer, const unsigned int length);
+};
+
+#endif /* MBR_hpp */
+*/

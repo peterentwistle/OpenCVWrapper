a491142b34c64f24b1dae90b7b502ef6fe403e92
Peter Entwistle <petertheanimator@gmail.com>
2017-04-21 22:53:48 +0100

    Copied files to source folder

--- Sources/OpenCVWrapper.mm
+++ Sources/OpenCVWrapper.mm
@@ -18,9 +18,17 @@ static int _color_BGR2GRAY = 6;
 
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
-CascadeClassifier happiness_cascade;
 
-cv::CascadeClassifier cascade;
+CascadeClassifier happiness_cascade;
+CascadeClassifier anger_cascade;
+CascadeClassifier contempt_cascade;
+CascadeClassifier fear_cascade;
+CascadeClassifier neutral_cascade;
+CascadeClassifier sadness_cascade;
+CascadeClassifier surprise_cascade;
+CascadeClassifier disgust_cascade;
+
+//cv::CascadeClassifier cascade;
 
 typedef struct VectorOfCvRects {
 	std::vector<cv::Rect> val = *new std::vector<cv::Rect>();
@@ -33,71 +41,56 @@ typedef struct CvMats {
 - (id)init {
     self = [super init];
     
-    NSBundle *bundle = [NSBundle mainBundle];
-    NSString *pathFace = [bundle pathForResource:@"haarcascade_frontalface_alt" ofType:@"xml"];
-    std::string faceCascadePath = (char *)[pathFace UTF8String];
-    
-    if (!face_cascade.load(faceCascadePath)) {
-        return nil;
-    }
+    // Load face cascade
+    [self loadCascadeClassifier: face_cascade fileName:@"haarcascade_frontalface_alt"];
 
-    NSString *pathEye = [bundle pathForResource:@"haarcascade_eye_tree_eyeglasses" ofType:@"xml"];
-    std::string eyeCascadePath = (char *)[pathEye UTF8String];
+    // Load eye cascade
+    [self loadCascadeClassifier: eyes_cascade fileName:@"haarcascade_eye_tree_eyeglasses"];
     
-    if (!eyes_cascade.load(eyeCascadePath)) {
-        return nil;
-    }
+    // Load Emotion classifiers
     
-    NSString *pathHappiness = [bundle pathForResource:@"happiness" ofType:@"xml"];
-    string happinessCascadePath = (char *)[pathHappiness UTF8String];
+    // Happiness
+    [self loadCascadeClassifier: happiness_cascade fileName:@"happiness"];
     
-    if (!happiness_cascade.load(happinessCascadePath)) {
-        return nil;
-    }
+    // Anger
+    [self loadCascadeClassifier: anger_cascade fileName:@"anger"];
+    
+    // Contempt
+    [self loadCascadeClassifier: contempt_cascade fileName:@"contempt"];
+    
+    //  Fear
+    [self loadCascadeClassifier: fear_cascade fileName:@"fear"];
+    
+    // Neutral
+    [self loadCascadeClassifier: neutral_cascade fileName:@"neutral"];
+    
+    // Sadness
+    [self loadCascadeClassifier: sadness_cascade fileName:@"sadness"];
+
+    // Surprise
+    [self loadCascadeClassifier: surprise_cascade fileName:@"surprise"];
+    
+    // Disgust
+    [self loadCascadeClassifier: disgust_cascade fileName:@"disgust"];
     
     return self;
 }
-/*
- Broken atm
-- (UIImage *)detectAndDisplay:(UIImage*)input {
-    VectorOfCvRect faces;
-    Mat frame_gray;
-    Mat frame;
-    
-    UIImageToMat(input, frame);
-    
-    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-    equalizeHist(frame_gray, frame_gray);
-	
-    //-- Detect faces
-    face_cascade.detectMultiScale(frame_gray, faces->val, 1.1, 2, 0|CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-    
-    for (size_t i = 0; i < faces->val.size(); i++) {
-        cv::Point center(faces->val[i].x + faces->val[i].width/2, faces->val[i].y + faces->val[i].height/2);
-        ellipse(frame, center, cv::Size(faces->val[i].width/2, faces->val[i].height/2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-        
-        Mat faceROI = frame_gray(faces->val[i]);
-        std::vector<cv::Rect> eyes;
-        
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-        
-        for (size_t j = 0; j < eyes.size(); j++) {
-            cv::Point eye_center(faces->val[i].x + eyes[j].x + eyes[j].width/2, faces->val[i].y + eyes[j].y + eyes[j].height/2);
-            int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-            circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);
-        }
-    }
-    //-- Show what you got
-    UIImage *resultImage = MatToUIImage(frame);
-    return resultImage;
-}
-*/
 
 void rotate90(cv::Mat &mat) {
     // Rotate matrix 90 degrees clockwise
     transpose(mat, mat);
     flip(mat, mat, 1);
+}
+
+- (void)loadCascadeClassifier:(CascadeClassifier&)cascade fileName:(NSString*)fileName {
+    NSBundle *bundle = [NSBundle mainBundle];
+    
+    NSString *path = [bundle pathForResource:fileName ofType:@"xml"];
+    string cascadePath = (char *)[path UTF8String];
+    
+    if (!cascade.load(cascadePath)) {
+        return nil;
+    }
 }
 
 - (DetectedResult *)detectAndDisplay:(UIImage*)input {
@@ -115,7 +108,7 @@ void rotate90(cv::Mat &mat) {
     // Only going to work for one emotion for now
     DetectedEmotion *detectedEmotion;
     
-    //-- Detect faces
+    // Detect faces
     face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, cv::Size(30, 30));
     
     for (size_t i = 0; i < faces.size(); i++) {
@@ -126,9 +119,9 @@ void rotate90(cv::Mat &mat) {
         std::vector<cv::Rect> eyes;
         
         // Detect emotions
-        detectedEmotion = [self detectHappiness:faceROI];
+        detectedEmotion = [self detectEmotion:faceROI];
         
-        //-- In each face, detect eyes
+        // In each face, detect eyes
         eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, cv::Size(30, 30));
         
         for (size_t j = 0; j < eyes.size(); j++) {
@@ -137,109 +130,88 @@ void rotate90(cv::Mat &mat) {
             circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);
         }
     }
-    //-- Show what you got
+    // Convert the matrix to a UIImage
     UIImage *resultImage = MatToUIImage(frame);
     
     DetectedResult *result = [[DetectedResult alloc] initWithDetectedEmotion:(detectedEmotion) frame:resultImage];
     return result;
 }
 
-- (DetectedEmotion *)detectHappiness:(Mat)frame {
-    std::vector<cv::Rect> happiness_faces;
-    
-    happiness_cascade.detectMultiScale(frame, happiness_faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-    
-    for (size_t i = 0; i < happiness_faces.size(); i++) {
-        printf("Happiness detected! At x: %d, y: %d width: %d height: %d \n", happiness_faces[i].x, happiness_faces[i].y, happiness_faces[i].width, happiness_faces[i].height);
-    }
-    
-    //frame: UIImage, emotion: Emotion, emotionFace: UIImage
+- (DetectedEmotion *)detectEmotion:(Mat)frame {
+    std::vector<cv::Rect> emotion_faces;
+
     Emotion emotion = EmotionNone;
     
-    if (happiness_faces.size() > 0) {
-        emotion = EmotionHappiness;
+    NSArray *emotions = @[@"happiness", @"anger", @"contempt", @"fear", @"neutral", @"sadness", @"surprise", @"disgust"];
+    
+    for (NSString *emotionName in emotions) {
+        
+        CascadeClassifier &cascade = [self getClassifier: emotionName];
+        
+        cascade.detectMultiScale(frame, emotion_faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+        
+        for (size_t i = 0; i < emotion_faces.size(); i++) {
+            printf("%s detected! At x: %d, y: %d width: %d height: %d \n", [emotionName UTF8String], emotion_faces[i].x, emotion_faces[i].y, emotion_faces[i].width, emotion_faces[i].height);
+        }
+        
+        if (emotion_faces.size() > 0) {
+            emotion = [self getEmotion: emotionName];
+        }
+        
+        if (emotion != EmotionNone) {
+            break;
+        }
+        
     }
     
     DetectedEmotion *detectedEmotion = [[DetectedEmotion alloc] initWithFrame:MatToUIImage(frame) emotion:emotion];
     return detectedEmotion;
 }
 
-/*
-- (UIImage *)detectAndDisplay:(UIImage*)input {
-    cv::Rect roi = [self detectROI: input];
-    cv::Mat image_roi;
-    
-    std::vector<cv::Rect> faces;
-    Mat frame_gray;
-    Mat frame;
-    
-    UIImageToMat(input, frame);
-
-    //video >> frame; // get a new frame from camera
-    
-    //if (frame.refcount == 0) break;
-    
-    //cv::Rect roi(458-50, 250-50, 320, 320);
-    
-    
-    image_roi = frame(roi);
-    
-    cvtColor(image_roi, frame_gray, COLOR_BGR2GRAY);
-    equalizeHist(frame_gray, frame_gray);
-    
-    //-- Detect faces
-    face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-    
-    for (size_t i = 0; i < faces.size(); i++) {
-        
-        // print out face location
-        printf("Face x: %d, y: %d width: %d height: %d \n", faces[i].x, faces[i].y, faces[i].width, faces[i].height);
-        
-        // Define ROI
-        //cv::Rect roi(faces[i].x, mfaces[i].y, faces[i].width, faces[i].height);
-        
-        cv::Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
-        ellipse(image_roi, center, cv::Size(faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-        //cv::rectangle(image_roi, cv::Point(faces[i].x, faces[i].y), cv::Point(faces[i].width + faces[i].x, faces[i].height + faces[i].y), Scalar(255, 0, 255), 4);
-        
-        Mat faceROI = frame_gray(faces[i]);
-        std::vector<cv::Rect> eyes;
-        
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-        
-        // Detect mouth region
-        if (eyes.size() == 2) {
-            cv::Point firstEyeCenter = [self centerOfEye:faces eyes:eyes faceNumber:i eyeNumber:0]; //(faces[i].x + eyes[0].x + eyes[0].width/2, faces[i].y + eyes[0].y + eyes[0].height/2);
-            cv::Point secondEyeCenter = [self centerOfEye:faces eyes:eyes faceNumber:i eyeNumber:1]; //cv::Point secondEyeCenter(faces[i].x + eyes[1].x + eyes[1].width/2, faces[i].y + eyes[1].y + eyes[1].height/2);
-            
-            int eyeDistance = secondEyeCenter.x - firstEyeCenter.x;
-            //printf("Eye distance is: %d", eyeDistance);
-            
-            int topMouth = 0.85 * eyeDistance;
-            int bottomMouth = 0.65 * eyeDistance;
-            int topMouthY = firstEyeCenter.y + topMouth;
-            int bottomMouthY = firstEyeCenter.y + bottomMouth + topMouth;
-            
-            cv::rectangle(image_roi, cv::Point(faces[i].x, topMouthY), cv::Point(faces[i].width + faces[i].x, bottomMouthY), Scalar(0, 255, 255), 4);
-        }
-        
-        for (size_t j = 0; j < eyes.size(); j++) {
-            cv::Point eye_center(faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2); // could use centerOfEye
-            int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-            circle(image_roi, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);
-        }
+- (Emotion)getEmotion:(NSString*)emotionName {
+    if ([emotionName isEqual: @"happiness"]) {
+        return EmotionHappiness;
+    } else if ([emotionName isEqual: @"anger"]) {
+        return EmotionAnger;
+    } /*else if ([emotionName isEqual: @"contempt"]) {
+        return EmotionContempt; // NEED TO ADD CONTEMPT
+    }*/ else if ([emotionName isEqual: @"fear"]) {
+        return EmotionFear;
+    } /*else if ([emotionName isEqual: @"neutral"]) {
+        return EmotionNeutral;
+    }*/
+    else if ([emotionName isEqual: @"sadness"]) {
+        return EmotionSadness;
+    } else if ([emotionName isEqual: @"surprise"]) {
+        return EmotionSurprise;
+    } else if ([emotionName isEqual: @"disgust"]) {
+        return EmotionDisgust;
     }
     
-    //imshow("Test", image_roi);
-    //if(waitKey(30) >= 0) break;
-    
-    
-    //-- Show what you got
-    UIImage *resultImage = MatToUIImage(image_roi);
-    return resultImage;
+    return EmotionNone;
 }
-*/
+
+- (CascadeClassifier&)getClassifier:(NSString*)emotionName {
+    if ([emotionName isEqual: @"happiness"]) {
+        return happiness_cascade;
+    } else if ([emotionName isEqual: @"anger"]) {
+        return anger_cascade;
+    } /*else if ([emotionName isEqual: @"contempt"]) {
+       return contempt_cascade; // NEED TO ADD CONTEMPT
+   }*/ else if ([emotionName isEqual: @"fear"]) {
+       return fear_cascade;
+   } /*else if ([emotionName isEqual: @"neutral"]) {
+      return neutral_cascade;
+   }*/else if ([emotionName isEqual: @"sadness"]) {
+       return sadness_cascade;
+   } else if ([emotionName isEqual: @"surprise"]) {
+       return surprise_cascade;
+   } else if ([emotionName isEqual: @"disgust"]) {
+       return disgust_cascade;
+   }
+
+}
+
 - (cv::Point)centerOfEye:(std::vector<cv::Rect>)faces eyes:(std::vector<cv::Rect>)eyes faceNumber:(int)faceNumber eyeNumber:(int)eyeNumber {
     return cv::Point (faces[faceNumber].x + eyes[eyeNumber].x + eyes[eyeNumber].width/2, faces[faceNumber].y + eyes[eyeNumber].y + eyes[eyeNumber].height/2);
 }
@@ -277,7 +249,6 @@ void rotate90(cv::Mat &mat) {
     return roi;
 }
 
-
 + (void)UIImageToMat:(UIImage*)input frame:(CvMats*)frame {
 	UIImageToMat(input, frame->val);
 }
@@ -289,86 +260,5 @@ void rotate90(cv::Mat &mat) {
 + (void)equalizeHist:(CvMats*)input output:(CvMats*)output {
 	equalizeHist(input->val, output->val);
 }
-
-
-/*
-// WORKING VERSION
-
- - (UIImage *)detectAndDisplay:(UIImage*)input {
-     std::vector<cv::Rect> faces;
-     Mat frame_gray;
-     Mat frame;
-     
-     UIImageToMat(input, frame);
-     
-     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-     equalizeHist(frame_gray, frame_gray);
-     
-     //-- Detect faces
-     face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-     
-     for (size_t i = 0; i < faces.size(); i++) {
-         cv::Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
-         ellipse(frame, center, cv::Size(faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-         
-         Mat faceROI = frame_gray(faces[i]);
-         std::vector<cv::Rect> eyes;
-         
-         //-- In each face, detect eyes
-         eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-         
-         for (size_t j = 0; j < eyes.size(); j++) {
-             cv::Point eye_center(faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2);
-             int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-             circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);
-         }
-     }
-     //-- Show what you got
-     UIImage *resultImage = MatToUIImage(frame);
-     return resultImage;
- }
-*/
-/*
-+ (UIImage *)recognizeFace:(UIImage *)image {
-    CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
-    CGFloat cols = image.size.width;
-    CGFloat rows = image.size.height;
-    
-    cv::Mat mat(rows, cols, CV_8UC4);
-    
-    CGContextRef contextRef = CGBitmapContextCreate(mat.data,
-                                                    cols,
-                                                    rows,
-                                                    8,
-                                                    mat.step[0],
-                                                    colorSpace,
-                                                    kCGImageAlphaNoneSkipLast |
-                                                    kCGBitmapByteOrderDefault);
-    
-    CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
-    CGContextRelease(contextRef);
-    
-    
-    std::vector<cv::Rect> faces;
-    cascade.detectMultiScale(mat, faces,
-                             1.1, 2,
-                             CV_HAAR_SCALE_IMAGE,
-                             cv::Size(30, 30));
-
-    std::vector<cv::Rect>::const_iterator r = faces.begin();
-    for(; r != faces.end(); ++r) {
-        cv::Point center;
-        int radius;
-        center.x = cv::saturate_cast<int>((r->x + r->width*0.5));
-        center.y = cv::saturate_cast<int>((r->y + r->height*0.5));
-        radius = cv::saturate_cast<int>((r->width + r->height) / 2);
-        cv::circle(mat, center, radius, cv::Scalar(80,80,255), 3, 8, 0 );
-    }
-    
-    UIImage *resultImage = MatToUIImage(mat);
-    
-    return resultImage;
-}
-*/
 
 @end
